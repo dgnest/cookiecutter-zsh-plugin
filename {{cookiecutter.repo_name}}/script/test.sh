f644deb83b7a5c599fbcdbf842fe0b1b573df0a2
@slovacus <slovacus@gmail.com>
2017-02-26 20:39:48 -0500

    !#6 Fix: info script test

--- {{cookiecutter.repo_name}}/script/test.sh
+++ {{cookiecutter.repo_name}}/script/test.sh
@@ -3,11 +3,3 @@
 
 # shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-cd "${SOURCE_DIR}" || echo 'Not Found'
-
-if [[ "$1" == "all" ]]; then
-    python manage.py test --settings="${DJANGO_SETTINGS_TEST_WITHOUT_DB}" 
-else
-    python manage.py test "$1" --settings="${DJANGO_SETTINGS_TEST_WITHOUT_DB}" 
-fi
